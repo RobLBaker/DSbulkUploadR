24780709ff7b17213cfffbcf1aea7df06be9c9bc
Robert Baker <robert_baker@nps.gov>
2025-08-21 20:45:13 -0600

    remove run validation checks

--- R/bulk_uploads.R
+++ R/bulk_uploads.R
@@ -32,48 +32,7 @@ assign("ds_dev_api",
 
 
 
-#file_size_error in GB
-check_input_file <- function(filename, path = getwd(),
-                             file_size_error = 100,
-                             file_number_error = 500) {
 
-  #check reference type:
-  check_ref_type(filename = filename, path = path)
-
-
-  check_files_exist(filename = filename, path = path)
-
-
-  check_file_number(filename,
-                    path = path,
-                    file_number_error = file_number_error)
-
-  check_file_size(filename,
-                  path = path,
-                  file_size_error = file_size_error)
-
-
-  check_508_format(filename = filename, path = path)
-
-  #check that dates are is ISO format:
-  check_start_date(filename = filename, path = path)
-  check_end_date(filename = filename, path = path)
-
-  #check dates occur at logical times:
-  check_end_after_start(filename = filename, path = path)
-  check_dates_past(filename = filename, path = path)
-
-
-  #check that producing units are valid units:
-  check_prod_units(filename = filename, path = path)
-
-  #check that content units are valid:
-  check_content_units(filename = filename, path = path)
-
-
-
-
-}
 
 #creates a draft reference; returns the new reference code:
 create_draft_reference <- function(draft_title = "Temp Title",
