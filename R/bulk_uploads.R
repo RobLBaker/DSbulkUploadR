d5bb594ef77cbb024e41b48addd5dff1a012ca46
Robert Baker <robert_baker@nps.gov>
2025-08-21 20:46:11 -0600

    remove utilities/helper functions

--- R/bulk_uploads.R
+++ R/bulk_uploads.R
@@ -1,33 +1,4 @@
-#setup/helper functions:
-#initiate new environment accessible from within package:
-.pkgglobalenv <- new.env(parent=emptyenv())
 
-#data_store API base URL:
-assign("ds_api",
-       "https://irmaservices.nps.gov/datastore/v7/rest/",
-       envir=.pkgglobalenv)
-
-#data_store secure API base URL:
-assign("ds_secure_api",
-       "https://irmaservices.nps.gov/datastore-secure/v7/rest/",
-       envir=.pkgglobalenv)
-
-#data_store dev api (requires secure)
-assign("ds_dev_api",
-       "https://irmadevservices.nps.gov/datastore-secure/v7/rest/",
-       envir = .pkgglobalenv)
-
-.ds_api <- function(x){
-  get("ds_api", envir = .pkgglobalenv)
-}
-
-.ds_secure_api <- function(x){
-  get("ds_secure_api", envir = .pkgglobalenv)
-}
-
-.ds_dev_api <- function(x){
-  get("ds_dev_api", envir = .pkgglobalenv)
-}
 
 
 
